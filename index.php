30f247f6bb8c51c2c931f4c7fd0ec41ea8913ef4
andy <werdnanoslen@gmail.com>
2015-01-17 01:16:27 -0500

    Added packages

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 
 try {
     $conn = new PDO (
-    "sqlsrv:server = tcp:kn8snbwiz9.database.windows.net,1433; Database = sweaterweather",
-    "sweaterweather",
-    'jwBE!*BkQGsu!rzspQBCj9T$u5QWCjTY'
-);
-$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        "sqlsrv:server = tcp:kn8snbwiz9.database.windows.net,1433; Database = sweaterweather",
+        "sweaterweather",
+        'jwBE!*BkQGsu!rzspQBCj9T$u5QWCjTY'
+    );
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     print("Error connecting to SQL Server.");
+    echo "<pre>";
     die(print_r($e));
+    echo "</pre>";
 }
 
 ?>

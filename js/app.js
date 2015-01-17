7fafb072c622f82ec17fccbfb77654069aa1036d
Stephen Song <stephen.song@gatech.edu>
2015-01-17 14:30:14 -0500

    Added basic info registration for client

--- js/app.js
+++ js/app.js
@@ -15,6 +15,10 @@ angular.module('sweaterweather', [
         templateUrl: 'partials/login.html',
         controller: 'LoginCtrl'
     }).
+    when('/register/client', {
+        templateUrl: 'partials/register/client/basic.html',
+        controller: 'RegisterCtrl'
+    }).
     when('/vis', {
         templateUrl: 'partials/vis.html',
         controller: 'AppCtrl'
